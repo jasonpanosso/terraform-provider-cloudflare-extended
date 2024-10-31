485a62cd9b23e69322e7911ddb1dfc58a4ef7171
jasonpanosso <jasonpanosso@protonmail.com>
2024-10-31 11:47:50 -0400

    init from template

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "devshell flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+
+      forEachSupportedSystem = f: nixpkgs.lib.genAttrs supportedSystems (system: f {
+        pkgs = import nixpkgs { inherit system; config.allowUnfree = true; };
+      });
+    in
+    {
+      devShells = forEachSupportedSystem
+        ({ pkgs }: {
+          default = pkgs.mkShell {
+            packages = [
+              pkgs.terraform
+              pkgs.go
+              pkgs.gnumake
+            ];
+          };
+        });
+    };
+}
